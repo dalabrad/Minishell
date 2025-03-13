fee36eb6ec4a7c9b72fc97fa47c2b35d1bd68171
David Labrador Gracia <dalabrad@c4r6s4.42urduliz.com>
2025-03-13 13:54:46 +0100

    aded src/minishell_builtins.c and modified inc/minishell_exec.h

--- inc/minishell_exec.h
+++ inc/minishell_exec.h
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 12:32:53 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/03/13 10:28:35 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:39:12 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 # include "../libft/inc/libft.h"
 # include "minishell_parsing.h"
 # include <stdio.h>
+
+typedef struct s_builtin
+{
+	char	*builtin_name;
+	int		(*foo)(char **args);
+}	t_builtin;
+
+//	src/minishell_builtins.c
+void	shell_exec(char **args);
 
 #endif
---
+++ src/minishell_builtins.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_builtins.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 12:38:55 by dalabrad          #+#    #+#             */
+/*   Updated: 2025/03/13 13:52:44 by dalabrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell_exec.h"
+#include "../inc/minishell_parsing.h"
+
+static int	shell_exit(char	**args)
+{
+	(void)args;
+	printf("Exiting ./minishel...\n");
+	exit(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
+}
+
+static int	shell_env(char **args)
+{
+	(void)args;
+	printf("executed shell_env().\n");
+	return (EXIT_SUCCESS);
+}
+
+static int	shell_echo(char **args)
+{
+	int		i;
+	bool	nl_flag;
+
+	i = 1;
+	nl_flag = true;
+	if (!args || !args[0])
+		return (EXIT_FAILURE);
+	if (args[i] && !ft_strncmp("-n", args[i], ft_strlen(args[i])))
+	{
+		nl_flag = false;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+	}
+	if (nl_flag)
+		printf("\n");
+	return (EXIT_SUCCESS);
+}
+
+t_builtin	g_builtin[] = {
+{.builtin_name = "exit", .foo = shell_exit},
+{.builtin_name = "env", .foo = shell_env},
+{.builtin_name = "echo", .foo = shell_echo},
+{.builtin_name = NULL},
+};
+
+void	shell_exec(char **args)
+{
+	int			i;
+	const char	*current;
+
+	i = 0;
+	while (g_builtin[i].builtin_name)
+	{
+		current = g_builtin[i].builtin_name;
+		if (!ft_strncmp(current, args[0], ft_strlen(args[0])))
+		{
+			g_builtin[i].foo(args);
+			return ;
+		}
+		i++;
+	}
+	printf("Aqui deberian implementarse los comandos de execve(%s)\n", args[0]);
+}
