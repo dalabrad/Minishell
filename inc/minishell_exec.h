8491ae5257265418219c593e4cc5d75b69981bb1
David Labrador Gracia <dalabrad@c4r6s4.42urduliz.com>
2025-03-20 14:46:17 +0100

    modified inc/minishell_exec.h

--- inc/minishell_exec.h
+++ inc/minishell_exec.h
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 12:32:53 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/03/20 14:30:58 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:45:35 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,22 @@ typedef struct s_envp
 
 //------BUILT-INS---------------------------
 
-//	src/minishell_cd.c
+//	src/built-ins/builtin_cd.c
 int		shell_cd(char **args);
 
-//	src/minishell_echo.c
+//	src/built-ins/builtin_echo.c
 int		shell_echo(char **args);
 
-//	src/minishell_env.c
+//	src/built-ins/builtin_env.c
 int		shell_env(char **args);
 
-//	src/minishell_exit.c
+//	src/built-ins/builtin_exit.c
 int		shell_exit(char	**args);
 
-//	src/minishell_export.c
+//	src/built-ins/builtin_export.c
 int		shell_export(char **args);
 
-//	src/minishell_pwd.c
+//	src/built-ins/builtin_pwd.c
 int		shell_pwd(char **args);
 
 //------------------------------------------
