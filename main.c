852040b0032b9f15eb38a79a5b2a62008aa75c0a
David Labrador Gracia <dalabrad@c4r6s4.42urduliz.com>
2025-03-20 17:13:13 +0100

    added main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 15:42:59 by dalabrad          #+#    #+#             */
+/*   Updated: 2025/03/20 15:44:37 by dalabrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell_exec.h"
+#include "minishell_parsing.h"
+
+int	main(void)
+{
+	printf("Este es un archivo de prueba :)\n");
+	return (EXIT_SUCCESS);
+}
