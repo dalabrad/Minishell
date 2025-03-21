cfb01af82ae4568212a6ef42a737688df8f65c95
David <dalabrad@student.42urduliz.com>
2025-03-21 19:22:20 +0100

    changed t_env->was_added to bool

--- inc/minishell_parsing.h
+++ inc/minishell_parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_parsing.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlorenzo <vlorenzo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 10:21:52 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/03/20 18:02:32 by vlorenzo         ###   ########.fr       */
+/*   Updated: 2025/03/21 19:20:54 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ typedef struct s_tokens
 
 typedef struct s_env
 {
-	int					was_added;
+	bool				was_added;
 	char				*name;
 	char				*value;
 	struct s_env		*next;
