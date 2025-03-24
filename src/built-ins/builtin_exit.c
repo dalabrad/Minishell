9915ef8b078c31fc9af4575d578d1f8085b22b5a
David <dalabrad@student.42urduliz.com>
2025-03-24 12:39:01 +0100

    modified exit builtin

--- src/built-ins/builtin_exit.c
+++ src/built-ins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:18:50 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/03/24 11:20:39 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/03/24 12:37:52 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 int	shell_exit(char	**args, t_env *shell_envp)
 {
 	(void)args;
-	printf("Exiting ./minishel...\n");
+	printf("Exiting ./minishell, freeing all allocated data...\n\n");
 	free_shell_envp_list(&shell_envp);
+	printf("Minishell: All data freed. Goodbye!!\n");
 	exit(EXIT_SUCCESS);
 	return (EXIT_SUCCESS);
 }
