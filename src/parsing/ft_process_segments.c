549c456ed24e7edd9e70e9881fb079867257bdc6
David Labrador Gracia <dalabrad@c4r7s2.42urduliz.com>
2025-05-10 18:01:05 +0200

    modified ft_process_segments.c including new idea in comments

--- src/parsing/ft_process_segments.c
+++ src/parsing/ft_process_segments.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:36:10 by vlorenzo          #+#    #+#             */
-/*   Updated: 2025/05/10 16:26:49 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/05/10 17:59:23 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,3 +50,51 @@ void process_segments(char **segments, t_tokens **tokens, size_t n)
 		i++;
 	}
 }
+
+
+/* #include "minishell_parsing.h"
+#include "minishell_exec.h"
+
+t_cmd token_to_cmd()
+{
+	t_cmd *tok_cmd;
+
+	tok_cmd = malloc(sizeof t_cmd);
+	noseque = process_single_segment();
+}
+
+t_tokens *process_single_segment(char *segment, t_tokens **token_ptr, size_t index)
+{
+	size_t segment_tokens = 0;
+	token_ptr[index] = check_args_fixed(segment, &segment_tokens);
+	if (!token_ptr[index])
+		fprintf(stderr, "Warning: could not tokenize segment %zu\n", index);
+	else
+	{
+		set_command_type(token_ptr[index]);
+		print_tokens(token_ptr[index]);
+	}
+	printf("Total tokens checking-process: %zu\n\n", segment_tokens);
+	return(token_ptr[index]);
+}
+
+void process_segments(char **segments, t_tokens **tokens, size_t n)
+{
+	t_cmd *tok_cmd;
+
+	tok_cmd = malloc(sizeof t_cmd);
+	size_t i = 0;
+	printf("\n=========== PIPE SEGMENTS ===========\n");
+	while (i < n)
+	{
+		if (!segments[i])
+		{
+			fprintf(stderr, "Error: pipe_segments[%zu] is NULL\n", i);
+			i++;
+			continue;
+		}
+		process_single_segment(segments[i], tokens, i);
+		tokens.str = 
+		i++;
+	}
+} */
