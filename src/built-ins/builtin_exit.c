b9c81f2f8ffb22c42a20c8ef16e4ee35dd1a7d8b
David Labrador Gracia <dalabrad@c4r7s4.42urduliz.com>
2025-08-05 19:17:33 +0200

    modified exit built-in

--- src/built-ins/builtin_exit.c
+++ src/built-ins/builtin_exit.c
@@ -3,11 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlorenzo <vlorenzo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:18:50 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/07/28 20:38:04 by vlorenzo         ###   ########.fr       */
-/*   Updated: 2025/07/28 21:43:48 by vlorenzo         ###   ########.fr       */
+/*   Updated: 2025/08/05 19:16:36 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +16,7 @@
 int	shell_exit(char **args, t_data *data)
 {
 	(void)args;
-	if (data && data->nbr_cmds == 1)
+	if (data && (data->nbr_cmds == 1 || data->nbr_cmds == 0))
 		printf("exit\n");
 	free_data(data);
 	free_cmd_list(data->first_cmd);
