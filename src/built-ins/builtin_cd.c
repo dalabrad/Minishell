80aa44e435a8279f89152f602575f2b8b2c12e39
David Labrador Gracia <dalabrad@c4r7s2.42urduliz.com>
2025-08-07 20:27:58 +0200

    modified cd builtin to include HOME case when no argument is used

--- src/built-ins/builtin_cd.c
+++ src/built-ins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:08:15 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/07/22 20:48:27 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/08/07 20:24:35 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,37 +32,44 @@ static int	export_pwd(t_data *data, char *pwd_path, char *pwd_type)
 	return (EXIT_SUCCESS);
 }
 
-static int	cd_check_args(char **args)
-{
-	if (array_size(args) != 1)
-		return (error_msg(CD_ERROR));
-	if (access(args[0], F_OK))
-		return (error_msg_arg(CD_NO_DIR, args[0]));
-	if (access(args[0], X_OK))
-		return (error_msg_arg(CD_NO_PERM, args[0]));
-	return (0);
-}
-
-int	shell_cd(char **args, t_data *data)
+static int	cd_execute(t_data *data, char *target)
 {
 	char	*old_pwd;
 	char	*pwd;
 
-	if (!args || !args[0])
-		return (EXIT_FAILURE);
-	if (cd_check_args(args))
-		return (EXIT_FAILURE);
 	old_pwd = getcwd(NULL, 0);
 	if (!old_pwd)
 		return (error_msg(MALLOC_ERROR));
-	if (chdir(args[0]))
-		return (free(old_pwd), error_msg(CHDIR_ERROR));
+	if (chdir(target))
+		return (free(old_pwd), free(target), error_msg(CHDIR_ERROR));
 	export_pwd(data, old_pwd, "OLDPWD=");
 	free(old_pwd);
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		return (error_msg(MALLOC_ERROR));
 	export_pwd(data, pwd, "PWD=");
-	free(pwd);
-	return (EXIT_SUCCESS);
+	return (free(pwd), free(target), EXIT_SUCCESS);
+}
+
+int	shell_cd(char **args, t_data *data)
+{
+	char	*target;
+
+	if (array_size(args) > 1)
+		return (error_msg(CD_ERROR));
+	if (!args[0])
+	{
+		target = get_shell_envp_value(data->shell_envp, "HOME");
+		if (!target)
+			return (ft_putstr_fd("minishell: cd: HOME not set\n", 2), 1);
+	}
+	else
+	{
+		if (access(args[0], F_OK))
+			return (error_msg_arg(CD_NO_DIR, args[0]));
+		if (access(args[0], X_OK))
+			return (error_msg_arg(CD_NO_PERM, args[0]));
+		target = ft_strdup(args[0]);
+	}
+	return (cd_execute(data, target));
 }
